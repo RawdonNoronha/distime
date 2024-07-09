4b06286f19061d55c20ada449fe4877cd1e7febf
RawdonNoronha <rawdonnoronha11@gmail.com>
2024-07-09 11:00:40 +0530

    Navigation Bar

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className='h-16 bg-slate-300'>
+      <div className='flex h-16'>
+        <div className='flex flex-none w-2/4 bg-red-700'>qdw</div>
+        <div className='flex flex-none w-2/4 bg-red-400'>wdqd</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
